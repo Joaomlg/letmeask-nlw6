be23b88d71eeb29a0b23c0d9f272eaf5c1efa078
joaomlg <joaomarcos1999jm@gmail.com>
2021-06-23 23:36:11 -0300

    like attrs added in useRoom hook

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type FirebaseQuestions = Record<string, {
   content: string,
@@ -9,7 +10,10 @@ type FirebaseQuestions = Record<string, {
     avatar: string
   },
   isHighlighted: boolean,
-  isAnswered: boolean
+  isAnswered: boolean,
+  likes: Record<string, {
+    authorId: string
+  }>
 }>
 
 type QuestionType = {
@@ -20,10 +24,13 @@ type QuestionType = {
     avatar: string
   },
   isHighlighted: boolean,
-  isAnswered: boolean
+  isAnswered: boolean,
+  likeCount: number,
+  likeId: string | undefined
 }
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
@@ -40,14 +47,20 @@ export function useRoom(roomId: string) {
           content: value.content,
           author: value.author,
           isHighlighted: value.isHighlighted,
-          isAnswered: value.isAnswered
+          isAnswered: value.isAnswered,
+          likeCount: Object.values(value.likes ?? {}).length,
+          likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
         }
       })
 
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
     })
-  }, [roomId])
+
+    return () => {
+      roomRef.off('value');
+    }
+  }, [roomId, user?.id])
 
   return { title, questions };
 }
