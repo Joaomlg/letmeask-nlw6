0a4878ccbee96fc2cd7d12801903b07ea2d88eb7
joaomlg <joaomarcos1999jm@gmail.com>
2021-06-23 22:36:17 -0300

    AdminRoom view implement

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,55 @@
+import { useParams } from 'react-router-dom';
+
+import { useRoom } from '../hooks/useRoom';
+
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+import { RoomCode } from '../components/RoomCode';
+import { Question } from '../components/Question';
+
+import '../styles/Room.scss';
+
+type RoomParams = {
+  id: string
+}
+
+export function AdminRoom () {
+  const params = useParams<RoomParams>();
+  
+  const roomId = params.id;
+  
+  const { title, questions } = useRoom(roomId);
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className="room-title">
+          <h1>Sala "{title}"</h1>
+          { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
+        </div>
+        
+        <div className="question-list">
+          {questions.map((question => {
+            return (
+              <Question 
+                key={question.id}
+                content={question.content} 
+                author={question.author} 
+              />
+            );
+          }))}
+        </div>
+      </main>
+    </div>
+  )
+}
