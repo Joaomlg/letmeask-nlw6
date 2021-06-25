58adbb50975cc9404cf2aa9a019e884b9c4a8c3c
joaomlg <joaomarcos1999jm@gmail.com>
2021-06-24 23:47:06 -0300

    set highlight and check answered buttons

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -5,6 +5,8 @@ import { useRoom } from '../hooks/useRoom';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
@@ -37,6 +39,18 @@ export function AdminRoom () {
     }
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true
+    });
+  }
+
   return (
     <div id="page-room">
       <header>
@@ -61,7 +75,19 @@ export function AdminRoom () {
                 key={question.id}
                 content={question.content} 
                 author={question.author} 
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button onClick={() => handleCheckQuestionAsAnswered(question.id)}>
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+                    <button onClick={() => handleHighlightQuestion(question.id)}>
+                      <img src={answerImg} alt="Dar descaque à pergunta" />
+                    </button>
+                  </>
+                )}
                 <button onClick={() => handleDeleteQuestion(question.id)}>
                   <img src={deleteImg} alt="Remover pergunta" />
                 </button>
